9acb2c221bbe56a26719c045454fb6a75149dc27
Samuel Achem <samuel.a@softcom.ng>
2020-06-23 14:16:40 +0100

    Initial Commit

---
+++ SafeBoda/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SafeBoda
+//
+//  Created by Achem Samuel on 6/23/20.
+//  Copyright © 2020 Achem Samuel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
